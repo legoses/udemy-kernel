028e536d4c2e2121edbb4308cbe5e1990743e653
suse <suse@linux.local>
2025-04-22 10:21:58 +0900

    beginning kernel implementation

---
+++ peachOS/src/memory/heap/heap.c
@@ -0,0 +1,6 @@
+#include "heap.h"
+
+// must pass an uninitialized heap, pass where you want heap to start, where you want heap to end, and a valid and filled in heap table
+int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *table) {
+    return 0;
+}
